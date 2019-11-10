539d4d834fbe898bf345ca8f8ccd2f58173eade0
Bartłomiej Płonka <bartlomiejplonka@gmail.com>
2019-11-10 17:38:30 +0100

    Add range selection box translations

--- src/main/java/pl/edu/agh/plonka/bartlomiej/diseasesdiagnoser/view/controller/RangeSelectorController.java
+++ src/main/java/pl/edu/agh/plonka/bartlomiej/diseasesdiagnoser/view/controller/RangeSelectorController.java
@@ -3,6 +3,7 @@ package pl.edu.agh.plonka.bartlomiej.diseasesdiagnoser.view.controller;
 import com.google.common.collect.BoundType;
 import com.google.common.collect.Range;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.CheckBox;
 import javafx.stage.Stage;
 import org.controlsfx.control.RangeSlider;
@@ -12,6 +13,7 @@ import static com.google.common.collect.BoundType.CLOSED;
 import static com.google.common.collect.BoundType.OPEN;
 import static com.google.common.collect.Range.range;
 import static java.lang.Math.round;
+import static pl.edu.agh.plonka.bartlomiej.diseasesdiagnoser.utils.binding.ObservableResourceFactory.getStringBinding;
 
 public class RangeSelectorController implements ResponseController<Range<Integer>> {
 
@@ -21,12 +23,24 @@ public class RangeSelectorController implements ResponseController<Range<Integer
     private CheckBox leftInclusiveCheckBox;
     @FXML
     private CheckBox rightInclusiveCheckBox;
+    @FXML
+    private Button okButton;
+    @FXML
+    private Button cancelButton;
 
     private Stage dialogStage;
     private boolean okClicked = false;
 
     @FXML
     private void initialize() {
+        bindTranslations();
+    }
+
+    private void bindTranslations() {
+        leftInclusiveCheckBox.textProperty().bind(getStringBinding("LEFT_INCLUSIVE"));
+        rightInclusiveCheckBox.textProperty().bind(getStringBinding("RIGHT_INCLUSIVE"));
+        okButton.textProperty().bind(getStringBinding("OK"));
+        cancelButton.textProperty().bind(getStringBinding("CANCEL"));
     }
 
     public void init(Stage dialogStage) {
